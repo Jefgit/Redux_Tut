d7cfa8e39daa947ac8695ef7ca06cc756e61490d
Jefgit <Riazondajf@gmail.com>
2023-09-05 10:53:24 +0800

    fetch data

--- react-rtk-demo/features/user/UserView.jsx
+++ react-rtk-demo/features/user/UserView.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchUser } from './userSlice'
 
 export const UserView = () => {
+  const dispatch = useDispatch()
+  const user = useSelector((state => state.user))
+  useEffect(()=> {
+    dispatch(fetchUser())
+  }, [])
+
   return (
     <div>
         <h2>List of Users</h2>
-        <ul>
-            
-        </ul>
+        {user.loading && <div>Loading...</div>}
+        {!user.loading && user.error ? <div><h2>Error: {user.error}</h2></div> : null}
+        {!user.loading && user.users.length ?  
+          <div>
+            <ul>
+              {user.users.map(user => <li key={user.id}>{user.name}</li>)}
+            </ul>
+          </div>
+          : null
+        }
+        
         <button>Fetch Users</button>
     </div>
   )
--- react-rtk-demo/features/user/userSlice.js
+++ react-rtk-demo/features/user/userSlice.js
@@ -10,7 +10,7 @@ const initialState = {
 export const fetchUser = createAsyncThunk('user/fetchUser', () => {
     return axios
     .get('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.data.map(user => user.id))
+    .then(response => response.data)
 })
 
 const userSlice = createSlice({
